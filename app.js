548b455c9405ace639c44cec06940698660b7525
manu <nkiliyumwami@gmail.com>
2021-12-17 16:07:29 -0500

    More challenges

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const priceStartDate=0.52
 const priceEndDate=0.59
+const product = 'Milk'
 
-const inflaRate = (priceStartDate, priceEndDate) => ((priceEndDate-priceStartDate)/priceStartDate) * 100 
+const inflaRate = (priceStartDate, priceEndDate , product) => (product ,  ((priceEndDate-priceStartDate)/priceStartDate) * 100 )
 
 
-console.log(inflaRate(0.52, 0.59))
+console.log('The inflation Rate of '+  product + ' is ' +  inflaRate( 0.52, 0.59, 'Milk')) 
